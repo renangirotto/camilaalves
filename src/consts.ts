bccd398d8231f65247e76a8ec031cde51d507da6
Renan Girotto <me.renangirotto+dev@gmail.com>
2025-04-03 22:17:17 -0300

    feat: add header

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const ROUTES = {
+  work: `/camilaalves#meu-trabalho`,
+  about: `/camilaalves#sobre-mim`,
+}
+
 export const SITE_TITLE = 'Camila Alves | UX Designer';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
